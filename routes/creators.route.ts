4abc4304b90260b0e69740ab9576f4760f46b721
Alison Luiz <alison_luiz@outlook.com.br>
2024-05-02 16:24:19 -0300

    feat: added role filter to get all creators

--- routes/creators.route.ts
+++ routes/creators.route.ts
@@ -5,6 +5,7 @@ import { errorMiddleware } from '../src/shared/middlewares/error.middleware'
 const creatorsRoutes = Router()
 
 creatorsRoutes.get('/creators', new CreatorController().getAll)
+creatorsRoutes.get('/creators/roles', new CreatorController().getRolesTypes)
 
 creatorsRoutes.use(errorMiddleware)
 
--- src/modules/creators/creator.controller.ts
+++ src/modules/creators/creator.controller.ts
@@ -1,17 +1,37 @@
 import { NextFunction, Request, Response } from "express";
+import { FindManyOptions } from "typeorm";
 import { NotFoundError } from "../../shared/helpers/api-erros";
 import { creatorRepository } from "./creator.repository";
+
 
 export class CreatorController {
   async getAll(req: Request, res: Response, next: NextFunction) {
     try {
-      const creators = await creatorRepository.find();
+      let options: FindManyOptions<any> = {};
 
-      if (!creators) {
+      Object.keys(req.query).forEach((param) => {
+        options.where = { ...options.where, [param]: req.query[param] };
+      });
+
+      const creators = await creatorRepository.find(options);
+
+      if (!creators || creators.length === 0) {
         throw new NotFoundError("Creators not found");
       }
 
       res.status(200).json(creators);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getRolesTypes(req: Request, res: Response, next: NextFunction) {
+    try {
+      const creators = await creatorRepository.find();
+      const roles = creators.map((creator) => creator.role);
+      const rolesTypes = [...new Set(roles)];
+
+      res.status(200).json(rolesTypes);
     } catch (error) {
       next(error);
     }
